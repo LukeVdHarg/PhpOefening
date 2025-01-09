ccc93731a444c679c62c9aa9ab6ff1056ae05956
Luke van der Harg <9025126@student.zadkine.nl>
2025-01-09 10:59:33 +0100

    Php week 2

---
+++ Week_2/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form action="welcome_get.php" method="GET">
+  Name: <input id="Naam" type="text" name="Naam"><br>
+  E-mail: <input id="Email" type="text" name="Email"><br>
+  Bericht: <input id="Bericht" type="text" name="Bericht"><br>
+<input type="submit">
+</form>
+</body>
+</html>
